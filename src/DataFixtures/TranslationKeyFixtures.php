e9fc12c64b22b17463453a7215e7c62938eb55f7
Sergey Lepesh <sergey.lepesh@gmail.com>
2021-03-28 21:06:52 +0300

    Add TranslationKey fixtures

---
+++ src/DataFixtures/TranslationKeyFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\TranslationKey;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TranslationKeyFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $keys = ['welcome', 'contacts', 'subscribe', 'team', 'careers', 'news', 'signin', 'signup', 'logout'];
+        foreach ($keys as $key) {
+            $translationKey = new TranslationKey();
+            $translationKey->setName('index.' . $key);
+            $manager->persist($translationKey);
+        }
+
+        $manager->flush();
+    }
+}
